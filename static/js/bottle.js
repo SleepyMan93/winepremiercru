58eba125bafff8c776bb89b6f096a8e4bdba209d
SleepyMan93 <w-donovan@hotmail.co.uk>
2022-06-02 18:37:21 +0000

    Added wine fill simulation animation on how it works page

---
+++ static/js/bottle.js
@@ -0,0 +1,54 @@
+function colorFill(element) {
+    let elementBox = element.getBoundingClientRect();
+    let distanceFromTop = +200;
+
+    if(elementBox.top - window.innerHeight < distanceFromTop) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function blobFill(element) {
+    let elementBox = element.getBoundingClientRect();
+    let distanceFromTop = -200;
+
+    if(elementBox.top - window.innerHeight < distanceFromTop) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function scanDocument() {
+    let pinkFill = document.querySelectorAll('.smoke');
+    let pinkBlob = document.querySelectorAll('.blob-smoke');
+    let brownFill = document.querySelectorAll('.no-pink')
+    let brownBlob = document.querySelectorAll('.blob-pink');
+
+    pinkFill.forEach(function(pink) {
+        if(colorFill(pink)) {
+            pink.classList.remove('smoke');
+        };
+    });
+
+    pinkBlob.forEach(function(fillPink) {
+        if(blobFill(fillPink)) {
+            fillPink.classList.remove('blob-smoke')
+        };
+    });
+
+    brownFill.forEach(function(brown) {
+        if(colorFill(brown)) {
+            brown.classList.remove('no-pink')
+        };
+    });
+
+    brownBlob.forEach(function(fillBrown) {
+        if(blobFill(fillBrown)) {
+            fillBrown.classList.remove('blob-pink')
+        };
+    });
+}
+
+document.addEventListener("scroll", _.throttle(scanDocument, 500));
