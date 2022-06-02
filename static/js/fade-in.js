49830c76912b2c44b11790b13ea818584cb4ba8b
SleepyMan93 <w-donovan@hotmail.co.uk>
2022-06-02 15:58:28 +0000

    Added some styling adjustments to homepage and translate animation to panel cards

--- static/js/fade-in.js
+++ static/js/fade-in.js
@@ -9,11 +9,37 @@ function isVisible(element) {
     }
 }
 
+function isMoveable(element) {
+    let elementBox = element.getBoundingClientRect();
+    let distanceFromTop = 0;
+
+    if(elementBox.top - window.innerHeight < distanceFromTop) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 function scanDocument() {
     let sectionList = document.querySelectorAll('.hidden');
+    let panelLeft = document.querySelectorAll('.left');
+    let panelRight = document.querySelectorAll('.right');
+
     sectionList.forEach(function(section) {
         if(isVisible(section)) {
             section.classList.remove('hidden');
+        };
+    });
+
+    panelLeft.forEach(function(move) {
+        if(isMoveable(move)) {
+            move.classList.remove('left');
+        };
+    });
+
+    panelRight.forEach(function(move) {
+        if(isMoveable(move)) {
+            move.classList.remove('right');
         };
     });
 }
